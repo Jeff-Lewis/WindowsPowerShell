2ac5013c3f07d8bb5a2792d55e5bef82efcfbf35
Daniel Steiner <dapstr@gmail.com>
2014-01-22 14:24:54 +0100

    New downloads to process

--- BatchVideoStreams.ps1
+++ BatchVideoStreams.ps1
@@ -4,20 +4,18 @@
 # Use on NAS
 # ffmpeg -i "http://svtplay10r-f.akamaihd.net/i/se/open/20130803/1128492-039A/IN_THE_NIGHT_GA-039A-968f11ea12a8bfd9_,900,348,564,1680,2800,.mp4.csmil/index_4_av.m3u8?null=&id=" -vcodec copy -strict experimental "/share/MD0_DATA/Multimedia/TV/2013-08-06.mp4"
 ###
-# GetVideoStream.ps1 -url '' -file 'C:\Users\dapst_000\Videos\.mp4'
+# GetVideoStream.ps1 -url '' -file 'C:\Users\Daniel\Videos\.mp4'
 ###
-# GetVideoStream.ps1 -url 'http://www.svtplay.se/dinosaurietaget' -file 'C:\Users\dapst_000\Videos\Ett apatosaurusäventyr.mp4'
-# GetVideoStream.ps1 -url 'http://www.svtplay.se/faret-shaun' -file 'C:\Users\dapst_000\Videos\Tandféen.mp4'
-# GetVideoStream.ps1 -url 'http://www.svtplay.se/hjarnkontoret' -file 'C:\Users\dapst_000\Videos\Hjärnkontoret – del 11 av 12.mp4'
-# # GetVideoStream.ps1 -url 'http://www.svtplay.se/lulu-zipadoo' -file 'C:\Users\dapst_000\Videos\.mp4'
-# GetVideoStream.ps1 -url 'http://www.svtplay.se/meckar-micke' -file 'C:\Users\dapst_000\Videos\Kurragömma.mp4'
-# # GetVideoStream.ps1 -url 'http://www.svtplay.se/molly-monster' -file 'C:\Users\dapst_000\Videos\.mp4'
-# # GetVideoStream.ps1 -url 'http://www.svtplay.se/oscars-oas' -file 'C:\Users\dapst_000\Videos\.mp4'
-# # GetVideoStream.ps1 -url '?' -file 'C:\Users\dapst_000\Videos\Pettson och Findus del 1 av 6.mp4'
-# GetVideoStream.ps1 -url 'http://www.svtplay.se/pettson-och-findus' -file 'C:\Users\dapst_000\Videos\Pettson och Findus del 3 av 6.mp4'
-# # GetVideoStream.ps1 -url 'http://www.svtplay.se/pingu' -file 'C:\Users\dapst_000\Videos\.mp4'
-###
-# GetVideoStream.ps1 -url 'http://www.svtplay.se/hitlatens-historia' -file 'C:\Users\dapst_000\Videos\Hitlåtens historia – Del 6 av 6.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/dinosaurietaget' -file 'C:\Users\Daniel\Videos\Ett apatosaurusäventyr.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/faret-shaun' -file 'C:\Users\Daniel\Videos\Tandféen.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/hjarnkontoret' -file 'C:\Users\Daniel\Videos\Hjärnkontoret – del 11 av 12.mp4'
+# # GetVideoStream.ps1 -url 'http://www.svtplay.se/lulu-zipadoo' -file 'C:\Users\Daniel\Videos\.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/meckar-micke' -file 'C:\Users\Daniel\Videos\Kurragömma.mp4'
+# # GetVideoStream.ps1 -url 'http://www.svtplay.se/molly-monster' -file 'C:\Users\Daniel\Videos\.mp4'
+# # GetVideoStream.ps1 -url 'http://www.svtplay.se/oscars-oas' -file 'C:\Users\Daniel\Videos\.mp4'
+# # GetVideoStream.ps1 -url '?' -file 'C:\Users\Daniel\Videos\Pettson och Findus del 1 av 6.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/pettson-och-findus' -file 'C:\Users\Daniel\Videos\Pettson och Findus del 3 av 6.mp4'
+# # GetVideoStream.ps1 -url 'http://www.svtplay.se/pingu' -file 'C:\Users\Daniel\Videos\.mp4'
 ###
 # Alla tiders hits http://www.svt.se/alla-tiders-hits/se-program//rss.xml?includeVideo=true
 # Musik special http://www.svt.se/musik-special/se-program//rss.xml?includeVideo=true
@@ -30,4 +28,9 @@
 # Dokumentärfilm http://www.svt.se/dokumentarfilm/se-program//rss.xml?includeVideo=true
 # DOX http://www.svt.se/dox/se-program//rss.xml?includeVideo=true
 # Uppdrag grankning http://www.svt.se/ug/se-program//rss.xml?includeVideo=true
+###
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/sherlock' -file 'C:\Users\Daniel\Videos\Sherlock – Säsong 1 del 3 av 3 – The Great Game.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/medicin-med-mosley' -file 'C:\Users\Daniel\Videos\Medicin med Mosley – Del 3 av 7 – Test av probiotika som många köper för dyra pengar.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/ironside' -file 'C:\Users\Daniel\Videos\Ironside – Del 1 av 9 – Test av probiotika som många köper för dyra pengar.mp4'
+# GetVideoStream.ps1 -url 'http://www.svtplay.se/fittstim---min-kamp' -file 'C:\Users\Daniel\Videos\Fittstim - min kamp – Del 1 av 3 – Har feminismen spårat ur.mp4'
 ###
